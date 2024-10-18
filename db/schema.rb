67d140818b9baf148b003e6c969372317110e252
Emerson Xavier <emerson.xavier@readytech.io>
2024-10-19 10:09:36 +1100

    rails upgrade

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_26_221258) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_18_230845) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
